40ef73bc9e563ed84b81f5a2e14caa5fb08f70b8
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-11-18 15:45:48 +0600

    L

---
+++ 800/10_CF236ABoyOrGirl.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cin >> name;
+
+    set<char> distinct_chars;
+    for (char c : name)
+    {
+        distinct_chars.insert(c);
+    }
+
+    int distinct_count = distinct_chars.size();
+
+    if (distinct_count % 2 == 0)
+    {
+        cout << "CHAT WITH HER!" << endl;
+    }
+    else
+    {
+        cout << "IGNORE HIM!" << endl;
+    }
+
+    return 0;
+}
