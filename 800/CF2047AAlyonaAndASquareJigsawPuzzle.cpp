ca18c36a36e9e0751ba66b165d7c69bab58221a2
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-12-11 07:49:05 +0600

    done

---
+++ 800/CF2047AAlyonaAndASquareJigsawPuzzle.cpp
@@ -0,0 +1,22 @@
+#include <iostream> // In the name of Allah, the Most Gracious, the Most Merciful
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+
+    while (tc--)
+    {
+        int days, happyDayNum = 0;
+        cin >> days;
+
+        while (days--)
+        {
+            int boxes;
+            cin >> boxes;
+        }
+    }
+
+    return 0;
+}
