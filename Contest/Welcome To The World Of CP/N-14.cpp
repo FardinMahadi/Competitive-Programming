386c8067763a39a36a57084d3bf7201b8edc2174
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-12-11 07:48:42 +0600

    done

--- Contest/Welcome To The World Of CP/N-14.cpp
+++ Contest/Welcome To The World Of CP/N-14.cpp
@@ -3,13 +3,22 @@ using namespace std;
 
 int main()
 {
-    int friends, bottles, milliliters, limes, slices, gram, drinkMl, saltG, toastCount = 0;
+    int n, k, l, c, d, p;
+    long int nl, np;
 
-    cin >> friends >> bottles >> milliliters >> limes >> slices >> gram >> drinkMl >> saltG;
+    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
 
-    if (true)
-    {
-    }
+    int totalLiquid = k * l;
+    int totalLimeSlices = c * d;
+    int totalSalt = p;
+
+    int toastsFromLiquid = totalLiquid / (n * nl);
+    int toastsFromSalt = totalSalt / (n * np);
+    int toastsFromLime = totalLimeSlices / n;
+
+    int maxToasts = min(toastsFromLiquid, min(toastsFromSalt, toastsFromLime));
+
+    cout << maxToasts << endl;
 
     return 0;
 }
