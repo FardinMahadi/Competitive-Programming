cd2d9250bd8382e25076ca5c47eaf28023eb7656
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-01-18 18:45:10 +0600

    update

---
+++ 800/CF977AWrongSubtraction.cpp
@@ -0,0 +1,23 @@
+// In the name of Allah, the Most Gracious, the Most Merciful
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 0; i < k; i++)
+    {
+        if (n % 10 == 0)
+        {
+            n /= 10;
+        }
+        else
+        {
+            n--;
+        }
+    }
+
+    cout << n << endl;
+}
