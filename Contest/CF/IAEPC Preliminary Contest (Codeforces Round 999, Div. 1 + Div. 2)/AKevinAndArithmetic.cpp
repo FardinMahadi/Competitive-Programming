40de13600a045bfdbc5c6e85c443fd4ed63d9571
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-01-20 22:06:02 +0600

    update

---
+++ Contest/CF/IAEPC Preliminary Contest (Codeforces Round 999, Div. 1 + Div. 2)/AKevinAndArithmetic.cpp
@@ -0,0 +1,26 @@
+// In the name of Allah, the Most Gracious, the Most Merciful
+#include <bits/stdc++.h>
+
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, s = 0, point = 0;
+        cin >> n;
+        vector<int> v(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            int value;
+            cin >> value;
+            v[i] = value;
+        }
+    }
+}
