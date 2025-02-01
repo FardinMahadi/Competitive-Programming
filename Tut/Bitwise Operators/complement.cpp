938f81801f525286c3e38a25b7e48d3663899567
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-02-01 13:13:13 +0600

    update

---
+++ Tut/Bitwise Operators/complement.cpp
@@ -0,0 +1,14 @@
+// In the name of Allah, the Most Gracious, the Most Merciful
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // n = -(n + 1)
+    int num1 = 35;
+    int num2 = -150;
+    cout << "~(" << num1 << ") = " << (~num1) << endl;
+    cout << "~(" << num2 << ") = " << (~num2) << endl;
+
+    return 0;
+}
---
+++ Tut/Bitwise Operators/or.cpp
@@ -0,0 +1,14 @@
+// In the name of Allah, the Most Gracious, the Most Merciful
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 12, b = 25;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "a | b = " << (a | b) << endl;
+
+    return 0;
+}
