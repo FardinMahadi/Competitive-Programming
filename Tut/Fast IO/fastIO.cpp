550303648deb8bc6715a4540294f03223d5fa7ff
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-02-09 18:32:36 +0600

    update

---
+++ Tut/Fast IO/fastIO.cpp
@@ -0,0 +1,19 @@
+// In the name of Allah, the Most Gracious, the Most Merciful
+#include <bits/stdc++.h>
+using namespace std;
+
+void fastIO()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int main()
+{
+    fastIO();
+
+    int n;
+    cin >> n;
+    cout << n << "\n";
+    return 0;
+}
