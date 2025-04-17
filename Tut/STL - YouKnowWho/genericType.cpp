1bbb17eaf2d1804f30d2d342d83f599fdaf0632a
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-04-17 10:37:56 +0600

    add

--- Tut/STL - YouKnowWho/genericType.cpp
+++ Tut/STL - YouKnowWho/genericType.cpp
@@ -4,11 +4,17 @@ using namespace std;
 
 // user-defined template function
 template <typename T>
-area(T a, T b){
-    return a*b;
+T area(T a, T b)
+{
+    return a * b;
 }
 
 int main()
 {
-    
+    int x1 = area<int>(static_cast<int>(10), 5);
+    double x2 = area<double>(10.5, 5);
+    float x3 = area<float>(10.5, 5);
+    cout << x1 << ", " << x2 << ", " << x3 << endl;
+
+    return 0;
 }
