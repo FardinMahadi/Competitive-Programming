a99f92f0d5a6cd7a1dc7a5d13e8c22d2a2fffe90
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-05-16 15:21:13 +0600

    add

--- Tut/Leveraging Monotonicity/Binary Search/BinSearch.cpp
+++ Tut/Leveraging Monotonicity/Binary Search/BinSearch.cpp
@@ -4,6 +4,29 @@ using namespace std;
 
 int main()
 {
+    string str;
+    cin >> str;
+
+    static map<char, int> list;
+
+    for (auto s : str)
+    {
+        auto it = list.find(s);
+
+        if (it != list.end())
+        {
+            it->second = it->second + 1;
+        }
+        else
+        {
+            list.insert({s, 1});
+        }
+    }
+
+    for (auto it : list)
+    {
+        cout << it.first << " : " << it.second << endl;
+    }
 
     return 0;
 }
