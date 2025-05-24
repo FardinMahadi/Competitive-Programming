a8bda704fc41ffa22dfa88c216ab8c21e26ecf4b
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-05-25 00:01:35 +0600

    add

--- Contest/CF/Codeforces Round 1026 (Div. 2)/AFashionableArray.cpp
+++ Contest/CF/Codeforces Round 1026 (Div. 2)/AFashionableArray.cpp
@@ -1,121 +1,67 @@
 // In the name of Allah, the Most Gracious, the Most Merciful
+// C: FardinMahadi
+
 #include <bits/stdc++.h>
 using namespace std;
 
-// ===== TYPES =====
-#define ll long long
-#define ld long double
-#define pii pair<int, int>
-#define pll pair<ll, ll>
+#define nl                  '\n'
+#define F                   first
+#define S                   second
+#define ll                  long long
+#define pb                  push_back
+#define MOD                 1000000007
+#define gcd(x,y)            __gcd(x,y)
+#define lcm(x,y)            y*x/__gcd(x,y)
+#define all(a)              (a.begin()),(a.end())
+#define SUM(a)              accumulate(all(a),0LL);
+#define ms(a,b)             memset(a, b, sizeof(a))
+#define UNIQUE(X)           (X).erase(unique(all(X)),(X).end())
+#define SORT_UNIQUE(c)      (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
 
-// ===== CONTAINERS =====
-#define vi vector<int>
-#define vl vector<ll>
-#define vii vector<pii>
-#define vll vector<pll>
+const double PI = acos(-1);
 
-// ===== SHORTCUTS =====
-#define pb push_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define all(x) (x).begin(), (x).end()
-#define sz(x) ((int)(x).size())
-
-// ===== ALGORITHMS =====
-#define srt(v) sort(all(v))
-#define rsrt(v) sort((v).rbegin(), (v).rend())
-#define rev(v) reverse(all(v))
-#define uniq(v) (v).erase(unique(all(v)), (v).end())
-#define min3(a, b, c) min(min(a, b), c)
-#define max3(a, b, c) max(max(a, b), c)
-#define min4(a, b, c, d) min(min(a, b), min(c, d))
-#define max4(a, b, c, d) max(max(a, b), max(c, d))
-
-// ===== LOOPS =====
-#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
-#define ROF(i, a, b) for (int i = (a); i >= (b); --i)
-#define REP(i, n) for (int i = 0; i < (n); ++i)
-
-// ===== IO + UTILS =====
-#define yes cout << "YES" << nl
-#define no cout << "NO" << nl
-#define nl '\n'
-#define sp ' '
-#define mem(a, b) memset(a, b, sizeof(a))
-#define tst    \
-    int TT;    \
-    cin >> TT; \
-    for (int tc = 1; tc <= TT; tc++)
-
-// ===== MATH HELPERS =====
-#define gcd(a, b) __gcd(a, b)
-
-// ===== CONSTANTS =====
-constexpr double PI = acos(-1);
-constexpr ll MOD = 1e9 + 7;
-constexpr ll INF = 1e18;
-
-void solve()
-{
+void Solve(int tc) {
     int n;
     cin >> n;
 
-    vector<int> arr(n);
-    for (auto &it : arr)
-    {
+    vector<int> v(n);
+    for(auto &it:v)
         cin >> it;
-    }
-    srt(arr);
 
-    size_t min = INT_MAX;
+    sort(all(v));
 
-    if (n == 1)
+    int i = 0, j = n-1;
+    int cnt1=0, cnt2=0;
+
+    while (i<n)
     {
-        if (arr[0] % 2 == 0)
-            cout << 0 << nl;
+        if((v[i]+v[n-1])%2)
+            cnt1++;
         else
-            cout << 1 << nl;
-        return;
+            break;
+        i++;
     }
 
-    if ((arr[0] + arr[n - 1]) % 2 == 0)
+    while (j>=1)
     {
-        cout << 0 << nl;
-        return;
-    }
-    else
-    {
-        if (arr[0] % 2 == 0)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                if (arr[i] % 2 == 1)
-                    min = i;
-                break;
-            }
-        }
+        if((v[0]+v[j])%2)
+            cnt2++;
         else
-        {
-            for (int i = n - 1; i >= 0; i--)
-            {
-                if (arr[i] % 2 == 0)
-                    if (min > n - 1 - i)
-                        min = n - 1 - i;
-                break;
-            }
-        }
+            break;
+        j--;
     }
-    cout << min << nl;
+
+    cout << min(cnt1, cnt2) << nl;
 }
 
-int main()
-{
-
-    tst
-    {
-        solve();
-    }
-
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t, T = 1;
+    cin >> T;
+    for(t = 1; t <= T; t++)
+        Solve(t);
+    
     return 0;
 }
