63636072c0fd737937ccf923a91860469c48a4e7
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-05-29 07:35:37 +0600

    add

---
+++ CodeForces/800/2106ADrTC.cpp
@@ -0,0 +1,56 @@
+// In the name of Allah, the Most Gracious, the Most Merciful
+// C: FardinMahadi
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sp                  ' '
+#define nl                  '\n'
+#define F                   first
+#define S                   second
+#define ll                  long long
+#define pb                  push_back
+#define MOD                 1000000007
+#define gcd(x,y)            __gcd(x,y)
+#define lcm(x,y)            y*x/__gcd(x,y)
+#define no                  cout << "NO" << nl
+#define yes                 cout << "YES" << nl
+#define all(a)              (a.begin()),(a.end())
+#define SUM(a)              accumulate(all(a),0LL);
+#define ms(a,b)             memset(a, b, sizeof(a))
+#define UNIQUE(X)           (X).erase(unique(all(X)),(X).end())
+#define print(v)            for(auto x : v) cout << x << " "; cout << nl
+#define SORT_UNIQUE(c)      (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
+
+const double PI = acos(-1);
+
+void Solve(int tc) {
+    size_t n;
+    string str;
+    cin >> n >> str;
+
+    size_t onesStr = 0, ones = 0;
+
+    for(int i = 0; i<n; ++i) {
+        if (str[i]=='1') onesStr++;
+    }
+
+    for( int i = 0; i<n; ++i){
+        if(str[i]=='1') ones += onesStr-1;
+        else ones += onesStr+1;
+    }
+
+    cout << ones << nl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t, T = 1;
+    cin >> T;
+    for(t = 1; t <= T; t++)
+        Solve(t);
+    
+    return 0;
+}
