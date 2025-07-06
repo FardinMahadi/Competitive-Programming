2412b385f871f3bde7fc12a7fa9f10fd6a0ff65e
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-07-06 06:31:13 +0600

    solution added

---
+++ CodeForces/800/2098AVadimsCollection.cpp
@@ -0,0 +1,67 @@
+// In the name of Allah, the Most Gracious, the Most Merciful
+// C: FardinMahadi
+
+/*  ______  __      __  ______  ____
+   / ____/ /  \    / / / |_) ) /   |
+  / /___  / /\ \  / / / /--<  /_/| |
+ / ____/ / /  \ \/ / / /_)  )   _| |_
+/_/     /_/    \__/ /_/____/   |_____| */
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define sp                  ' '
+#define nl                  '\n'
+#define F                   first
+#define S                   second
+#define ll                  long long
+#define pb                  push_back
+#define gcd(x,y)            __gcd(x,y)
+#define lcm(x,y)            y*x/__gcd(x,y)
+#define no                  cout << "NO" << nl
+#define yes                 cout << "YES" << nl
+#define all(a)              (a.begin()),(a.end())
+#define SUM(a)              accumulate(all(a),0LL)
+#define UNIQUE(X)           (X).erase(unique(all(X)),(X).end())
+#define print(v)            for(auto x : v) cout << x << " "; cout << nl
+#define SORT_UNIQUE(c)      (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
+
+const double PI = acos(-1);
+
+void Solve(int tc) {
+    string num; cin >> num;
+
+    vector<char> digits;
+    for (int i = 0; i < 10; ++i) digits.pb(num[i]);
+
+    sort(all(digits));
+
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < digits.size(); ++j) {
+            if ((digits[j] - '0') >= 9 - i) {
+                num[i] = digits[j];
+                digits.erase(digits.begin() + j);
+                break;
+            }
+        }
+    }
+
+    cout << num << nl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) Solve(tc);
+    
+    return 0;
+}
